4f483fd51a951b06e3c9b2c165ef89d904c131f9
alphadevvv <arthurcho72@gmail.com>
2020-03-22 23:51:15 +0100

    renderer is now ready to draw beautiful music sheets

--- app/music-renderer.js
+++ app/music-renderer.js
@@ -1,14 +1,14 @@
 VF = Vex.Flow;
 var context;
 
-var treble_y = 0;
-var bass_y = 0;
+var treble_current_y = 0;
+var bass_current_y = 0;
 
 var treble_current_x = 0;
 var bass_current_x = 0;
 
-var treble_staves = [];
-var bass_staves = [];
+var current_line = 0;
+var lines = [];
 
 var measures_by_line = 4;
 var measure_width = 300;
@@ -24,14 +24,20 @@ function draw_score(div_id, w, h) {
 
   renderer.resize(w, h);
   context = renderer.getContext();
+  context.scale(0.7, 0.7);
 }
 
 function draw_initial_measures(treble_notes, bass_notes) {
-  var stave_treble = new VF.Stave(50, 10, measure_width);
-  var stave_bass = new VF.Stave(50, 110, measure_width);
+  var stave_treble = new VF.Stave(50, current_line * 250 + 10, measure_width);
+  var stave_bass = new VF.Stave(50, current_line * 250 + 110, measure_width);
 
-  stave_treble.addClef("treble").addTimeSignature("4/4").addKeySignature("G").setContext(context);
-  stave_bass.addClef("bass").addTimeSignature("4/4").setContext(context);
+  stave_treble.addClef("treble").addKeySignature("G").setContext(context);
+  stave_bass.addClef("bass").setContext(context);
+
+  if(current_line == 0) {
+    stave_treble.addTimeSignature("4/4");
+    stave_bass.addTimeSignature("4/4");
+  }
 
   var connector = new VF.StaveConnector(stave_treble, stave_bass);
   connector.setType(VF.StaveConnector.type.BRACE);
@@ -46,11 +52,22 @@ function draw_initial_measures(treble_notes, bass_notes) {
   connector.draw();
   line.draw();
 
-  treble_y = stave_treble.y;
+  treble_current_y = stave_treble.y;
   treble_current_x = stave_treble.width + stave_treble.x;
 
-  bass_y = stave_bass.y;
+  bass_current_y = stave_bass.y;
   bass_current_x = stave_bass.width + stave_bass.x;
+
+  if(treble_notes == null) {
+    treble_notes = [
+      new VF.StaveNote({keys: ["b/4"], duration: "1r", align_center: true }),
+    ];
+  }
+  if(bass_notes == null) {
+    bass_notes = [
+      new VF.StaveNote({keys: ["b/4"], duration: "1r", align_center: true }),
+    ];
+  }
 
   var tbeams = VF.Beam.generateBeams(treble_notes);
   var bbeams = VF.Beam.generateBeams(bass_notes);
@@ -61,8 +78,7 @@ function draw_initial_measures(treble_notes, bass_notes) {
   tbeams.forEach(function(b) {b.setContext(context).draw()})
   bbeams.forEach(function(b) {b.setContext(context).draw()})
 
-  treble_staves.push(stave_treble);
-  bass_staves.push(stave_bass);
+  lines.push({treble: [stave_treble], bass: [stave_bass]});
 }
 
 function draw_measure(treble_notes, bass_notes) {
@@ -71,11 +87,15 @@ function draw_measure(treble_notes, bass_notes) {
     return;
   }
 
-  var width = 300;
-  var stave;
+  if(lines[current_line].treble.length >= measures_by_line) {
+    draw_line_ending_bar(current_line);
+    current_line += 1;
+    draw_initial_measures(treble_notes, bass_notes);
+    return;
+  }
 
-  tstave = new VF.Stave(treble_current_x, treble_y, measure_width);
-  bstave = new VF.Stave(bass_current_x, bass_y, measure_width);
+  var tstave = new VF.Stave(treble_current_x, treble_current_y, measure_width);
+  var bstave = new VF.Stave(bass_current_x, bass_current_y, measure_width);
 
   tstave.setContext(context).draw();
   bstave.setContext(context).draw();
@@ -103,21 +123,33 @@ function draw_measure(treble_notes, bass_notes) {
   treble_current_x = tstave.width + tstave.x;
   bass_current_x = bstave.width + bstave.x;
 
-  treble_staves.push(tstave);
-  bass_staves.push(bstave);
+  lines[current_line].treble.push(tstave);
+  lines[current_line].bass.push(bstave);
 }
 
-function draw_ending_bar() {
-  if(treble_staves.length == 0) {
+function draw_line_ending_bar(line) {
+  if(lines[line].treble.length == 0) {
     return;
   }
 
-  var connector = new VF.StaveConnector(treble_staves[treble_staves.length-1], bass_staves[bass_staves.length-1]);
+  var connector = new VF.StaveConnector(lines[line].treble[lines[line].treble.length-1], lines[line].bass[lines[line].bass.length-1]);
+  connector.setType(VF.StaveConnector.type.SINGLE_RIGHT);
+  connector.setContext(context);
+  connector.draw();
+}
+
+function draw_ending_bar() {
+  if(lines[current_line].treble.length == 0) {
+    return;
+  }
+
+  var connector = new VF.StaveConnector(lines[current_line].treble[lines[current_line].treble.length-1], lines[current_line].bass[lines[current_line].bass.length-1]);
   connector.setType(VF.StaveConnector.type.BOLD_DOUBLE_RIGHT);
   connector.setContext(context);
   connector.draw();
 };
 
+// TEMP: testing this
 var tnotes = [
   new VF.StaveNote({clef: "treble", keys: ["c/5"], duration: "8" }),
   new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "16" }),
@@ -135,8 +167,13 @@ var bnotes = [
   new VF.StaveNote({clef: "bass", keys: ["b/3"], duration: "q" }),
 ];
 
-draw_score("score", 1000, 1000);
+draw_score("score", 2000, 5000);
 draw_measure(tnotes, bnotes);
 draw_measure(tnotes, null);
 draw_measure(null, bnotes);
+draw_measure(tnotes, bnotes);
+draw_measure(tnotes, bnotes);
+draw_measure(tnotes, null);
+draw_measure(null, bnotes);
+draw_measure(tnotes, bnotes);
 draw_ending_bar();
