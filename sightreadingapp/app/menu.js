3c75a6a22a3850509e0d389793777fb97682bbab
alphadevvv <arthurcho72@gmail.com>
2020-03-23 19:44:44 +0100

    menu

--- sightreadingapp/app/menu.js
+++ sightreadingapp/app/menu.js
@@ -1,3 +1,37 @@
+instrument = "no-instrument";
+level = 0;
+timesig = "4/4";
+scale = "C Major";
+
+tabs = ["instrument", "level", "timesig", "scale"];
+
+current_tab = 0;
+
+function pick_instrument(event) {
+  instrument = event.target.id;
+
+  next_tab();
+}
+
+function next_tab() {
+  if(current_tab == (tabs.length-1)) {
+    validate();
+  }
+
+  current_tab += 1;
+
+  $("#tab-" + tabs[current_tab]).addClass("");
+
+  $("#pick-" + tabs[current_tab]).show();
+  if(current_tab > 0) {
+    $("#pick-" + tabs[current_tab-1]).hide();
+  }
+}
+
+function validate() {
+  alert("validate");
+}
+
 $(function () {
-  
+  $("#pick-instrument .element").click(pick_instrument);
 });
--- sightreadingapp/tests/test.js
+++ sightreadingapp/tests/test.js
@@ -1,53 +1,16 @@
-VF = Vex.Flow;
+var struct = {
+  var1: "hdjksdf",
+  baj: 456
+};
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var div = document.getElementById("score")
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-renderer.resize(1000, 1000);
-var context = renderer.getContext();
-// Create a stave at position 10, 40 of width 400 on the canvas.
-var stave_treble = new VF.Stave(10, 10, 300);
-var stave_bass = new VF.Stave(10, 110, 400);
+hfks(function() {...})
 
-// Add a clef and time signature.
-stave_treble.addClef("treble").addTimeSignature("4/4").addKeySignature("G").setContext(context);
-stave_bass.addClef("bass").addTimeSignature("4/4").setContext(context);
+(function() {...})();
 
-var connector = new VF.StaveConnector(stave_treble, stave_bass);
-connector.setType(VF.StaveConnector.type.BRACE);
-connector.setText('Piano');
-connector.setContext(context);
+var jdsfk = function() {
 
-var line = new VF.StaveConnector(stave_treble, stave_bass);
-line.setType(VF.StaveConnector.type.SINGLE);
-line.setContext(context);
+}
 
-stave_treble.draw();
-stave_bass.draw();
-connector.draw();
-line.draw();
+function jdsfk() {
 
-var stave2 = new VF.Stave(stave_treble.width + stave_treble.x, stave_treble.y, 300);
-stave2.setContext(context).draw();
-
-var stave3 = new VF.Stave(stave_bass.width + stave_bass.x, stave_bass.y, 300);
-stave3.setContext(context).draw();
-
-var notes = [
-  new VF.StaveNote({clef: "treble", keys: ["c/5"], duration: "q" }).addAccidental(0, new VF.Accidental("n")).addDotToAll(),
-  new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
-  new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
-  new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" }),
-];
-
-VF.Formatter.FormatAndDraw(context, stave_treble, notes);
-
-var notes2 = [
-  new VF.StaveNote({clef: "treble", keys: ["e/4"], duration: "h" }),
-  new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
-  new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" }),
-];
-
-VF.Formatter.FormatAndDraw(context, stave2, notes2);
-
-VF.Formatter.FormatAndDraw(context, stave3, notes2);
+}
