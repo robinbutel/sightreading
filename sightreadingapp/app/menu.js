2d409cb5ddb7c661f2ed5a7968ded7db5d26d9ae
alphadevvv <arthurcho72@gmail.com>
2020-03-23 21:23:52 +0100

    changing menu

--- sightreadingapp/app/menu.js
+++ sightreadingapp/app/menu.js
@@ -13,6 +13,12 @@ function pick_instrument(event) {
   next_tab();
 }
 
+function pick_level(event) {
+  levelid = event.target.id;
+
+  next_tab();
+}
+
 function next_tab() {
   if(current_tab == (tabs.length-1)) {
     validate();
@@ -20,7 +26,10 @@ function next_tab() {
 
   current_tab += 1;
 
-  $("#tab-" + tabs[current_tab]).addClass("");
+  $("#tab-" + tabs[current_tab] + " .tab-item").addClass("active");
+  if(current_tab > 0) {
+    $("#tab-" + tabs[current_tab-1] + " .tab-item").removeClass("active");
+  }
 
   $("#pick-" + tabs[current_tab]).show();
   if(current_tab > 0) {
@@ -32,6 +41,13 @@ function validate() {
   alert("validate");
 }
 
+function tab_goto(event) {
+
+}
+
 $(function () {
   $("#pick-instrument .element").click(pick_instrument);
+  $("#pick-level .element").click(pick_level);
+
+  $("#navbar-menu .tab").click(tab_goto);
 });
