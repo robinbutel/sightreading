d40ba915e23e3bfe93d3922bd32e7f6a5ee6b1c1
robinbutel <robinbutel@hotmail.com>
2020-03-23 20:54:20 +0100

    start of something great

---
+++ sightreadingapp/note_generation_src/gamme_base.js
@@ -0,0 +1,15 @@
+var cmaj_rh = ["c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5"];
+var cmaj_lh = ["c/3", "d/3", "e/3", "f/3", "g/3", "a/3", "b/3", "c/5"];
+
+function random(max)
+{
+   return Math.floor(Math.random() * (max+1));
+}
+
+function first_mesure()
+{
+    var tnotes = [];
+    var bnotes = [];
+    var notes = [tnotes,bnotes];
+    
+}
