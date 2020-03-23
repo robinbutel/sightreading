908b8e36ed03e722a263f969f346fb1f333048e9
alphadevvv <arthurcho72@gmail.com>
2020-03-23 22:51:36 +0100

    changing menu

--- sightreadingapp/app/menu.js
+++ sightreadingapp/app/menu.js
@@ -26,9 +26,9 @@ function next_tab() {
 
   current_tab += 1;
 
-  $("#tab-" + tabs[current_tab] + " .tab-item").addClass("active");
+  $("#tab-" + tabs[current_tab]).addClass("active");
   if(current_tab > 0) {
-    $("#tab-" + tabs[current_tab-1] + " .tab-item").removeClass("active");
+    $("#tab-" + tabs[current_tab-1]).removeClass("active");
   }
 
   $("#pick-" + tabs[current_tab]).show();
@@ -41,26 +41,32 @@ function validate() {
   alert("validate");
 }
 
-function tab_goto(event) {
+function tab_goto(goto_tab) {
+  last_tab = current_tab;
+  current_tab = goto_tab;
+
+  $("#tab-" + tabs[current_tab]).addClass("active");
+  $("#tab-" + tabs[last_tab]).removeClass("active");
+
+  $("#pick-" + tabs[current_tab]).show();
+  $("#pick-" + tabs[last_tab]).hide();
+}
+
+function tab_onclick(event) {
   goto_id = event.target.id.split("-")[1];
   if (goto_id == null) {
       goto_id = event.target.parentElement.id.split("-")[1];
   }
   goto_tab = tabs.indexOf(goto_id);
 
-  last_tab = current_tab;
-  current_tab = goto_tab;
-
-  $("#tab-" + tabs[current_tab] + " .tab-item").addClass("active");
-  $("#tab-" + tabs[last_tab] + " .tab-item").removeClass("active");
-
-  $("#pick-" + tabs[current_tab]).show();
-  $("#pick-" + tabs[last_tab]).hide();
+  tab_goto(goto_tab);
 }
 
 $(function () {
   $("#pick-instrument .element").click(pick_instrument);
   $("#pick-level .element").click(pick_level);
 
-  $("#navbar-menu .tab-item").click(tab_goto);
+  $("#navbar-menu .tab-item").click(tab_onclick);
+
+  tab_goto(1);
 });
