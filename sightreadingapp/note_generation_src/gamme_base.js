927f682f68efcc98f2ced0c9c13e988feafc0356
robinbutel <robinbutel@hotmail.com>
2020-03-23 21:13:15 +0100

    bug fix

--- sightreadingapp/note_generation_src/gamme_base.js
+++ sightreadingapp/note_generation_src/gamme_base.js
@@ -1,5 +1,5 @@
 var cmaj_rh = ["c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5"];
-var cmaj_lh = ["c/3", "d/3", "e/3", "f/3", "g/3", "a/3", "b/3", "c/5"];
+var cmaj_lh = ["c/3", "d/3", "e/3", "f/3", "g/3", "a/3", "b/3", "c/4"];
 var temp = ["","w","h","q","q"];
 
 function random(max)
