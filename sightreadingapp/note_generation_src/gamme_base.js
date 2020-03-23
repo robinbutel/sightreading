9f948a6d511c504f1841779f91fa3c1fef545f34
robinbutel <robinbutel@hotmail.com>
2020-03-23 21:14:23 +0100

    bug fix

--- sightreadingapp/note_generation_src/gamme_base.js
+++ sightreadingapp/note_generation_src/gamme_base.js
@@ -2,6 +2,8 @@ var cmaj_rh = ["c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5"];
 var cmaj_lh = ["c/3", "d/3", "e/3", "f/3", "g/3", "a/3", "b/3", "c/4"];
 var temp = ["","w","h","q","q"];
 
+var VF = Vex.Flox;
+
 function random(max)
 {
    return Math.floor(Math.random() * (max+1));
@@ -16,11 +18,11 @@ function first_mesure(tnum,bnum)
     var i;
     for(i=0; i<tnum; i++)
     {
-        tnotes.push(VF.StaveNote({clef: "treble", keys: [cmaj_rh[random(7)]], duration: temp[tnum]}));
+        tnotes.push(new VF.StaveNote({clef: "treble", keys: [cmaj_rh[random(7)]], duration: temp[tnum]}));
     }
     for(i=0; i<bnum; i++)
     {
-        bnotes.push(VF.StaveNote({clef: "bass", keys: [cmaj_lh[random(7)]], duration: temp[bnum]}));
+        bnotes.push(new VF.StaveNote({clef: "bass", keys: [cmaj_lh[random(7)]], duration: temp[bnum]}));
     }
 
     return notes;
