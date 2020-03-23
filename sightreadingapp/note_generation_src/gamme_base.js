68ca6ddbec953762c5b4368feafd06df491bb382
robinbutel <robinbutel@hotmail.com>
2020-03-23 23:08:23 +0100

    update to options.txt

--- sightreadingapp/note_generation_src/gamme_base.js
+++ sightreadingapp/note_generation_src/gamme_base.js
@@ -1,7 +1,9 @@
 var cmaj_rh = ["c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5"];
 var cmaj_lh = ["c/3", "d/3", "e/3", "f/3", "g/3", "a/3", "b/3", "c/4"];
-var temp = ["","w","h","q","8","16"];
+var lengths = ["w","h","q","8","16"];
+var notebeats = [1,1/2,1/4,1/8,1/16];
 
+var temp = ["","w","h","q","8","16"];
 
 
 var VF = Vex.Flox;
@@ -30,19 +32,27 @@ function first_mesure(tnum,bnum)
     return notes;
 }
 
-function get_mesure(time_signature, scale, variety)
+function get_mesure(beats, t, scale, variety)
 {
     var tnotes = [];
     var bnotes = [];
     var notes = {treble:tnotes,bass:bnotes};
-    var tbeats = time_signature[0];
-    var bbeats = time_signature[0];
-
+    var tbeats = t;
+    var bbeats = t;
+    
 }
 
 function get_notes(time_signature, scale, variety, independence, position, octave, chords, leap, accidental)
 {
+    var tbeats = time_signature[0];
+    var bbeats = time_signature[0];
+    var timedbeats,i;
+    for(i=0; i<=variety; i++)
+    {
+        timedbeats = notebeats[i] * time_signature[2];
+    }
 
+    get_mesure(timedbeats, time_signature[0], scale, variety);
 }
 
 // new VF.StaveNote({clef: "bass", keys: ["d/3"], duration: "h"})
