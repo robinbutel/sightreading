c1a2602bd245eda3ca9c8a2de00e9515b0c2fd11
alphadevvv <arthurcho72@gmail.com>
2020-03-24 12:01:15 +0100

    more buttons !

--- sightreadingapp/app/app.js
+++ sightreadingapp/app/app.js
@@ -16,46 +16,8 @@ function play_score() {
 }
 
 $(function(){
-  $("#maximize-btn").click(toggleFullScreen);
-  $("#minimize-btn").click(toggleFullScreen);
-
   $("#next-btn").click(next_score);
 
   $("#pause-btn").click(play_score);
   $("#play-btn").click(play_score);
 });
-
-fullscreen = false;
-function toggleFullScreen() {
-  if(fullscreen) {
-    $("#maximize-btn").show();
-    $("#minimize-btn").hide();
-  } else {
-    $("#maximize-btn").hide();
-    $("#minimize-btn").show();
-  }
-  fullscreen = !fullscreen;
-
-  if (!document.fullscreenElement &&    // alternative standard method
-      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
-    if (document.documentElement.requestFullscreen) {
-      document.documentElement.requestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) {
-      document.documentElement.msRequestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) {
-      document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) {
-      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-    }
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    }
-  }
-}
---
+++ sightreadingapp/app/style.js
@@ -0,0 +1,39 @@
+$(function(){
+  $("#maximize-btn").click(toggleFullScreen);
+  $("#minimize-btn").click(toggleFullScreen);
+});
+
+fullscreen = false;
+function toggleFullScreen() {
+  if(fullscreen) {
+    $("#maximize-btn").show();
+    $("#minimize-btn").hide();
+  } else {
+    $("#maximize-btn").hide();
+    $("#minimize-btn").show();
+  }
+  fullscreen = !fullscreen;
+
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
