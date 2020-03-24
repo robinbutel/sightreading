2b028d1d800d5cdae24ff19b716f1667b1516718
alphadevvv <arthurcho72@gmail.com>
2020-03-24 11:28:43 +0100

    more custom inputs

--- sightreadingapp/style/custom-elements.js
+++ sightreadingapp/style/custom-elements.js
@@ -27,6 +27,8 @@ $(function () {
 
       let menu = $(this);
 
+      $('.select-menu').removeClass('open');
+
       if(!menu.hasClass('open')) {
           menu.addClass('open');
       }
@@ -60,14 +62,34 @@ $(function () {
       }
   });
 
-  //This is sort of CSS-only; the JS below just sticks a span around each letter, so i can animate each independantly.
-  //(oh for an :nth-letter selector!)
-  const labels = document.querySelectorAll('.label');
-  labels.forEach(label => {
-    const chars = label.textContent.split('');
-    label.innerHTML = '';
-    chars.forEach(char => {
-      label.innerHTML += `<span>${char === ' ' ? '&nbsp' : char}</span>`;
-    });
-});
+  $(".drop .option").click(function() {
+    var val = $(this).attr("data-value"),
+        $drop = $(".drop"),
+        prevActive = $(".drop .option.active").attr("data-value"),
+        options = $(".drop .option").length;
+    $drop.find(".option.active").addClass("mini-hack");
+    $drop.toggleClass("visible");
+    $drop.removeClass("withBG");
+    $(this).css("top");
+    $drop.toggleClass("opacity");
+    $(".mini-hack").removeClass("mini-hack");
+    if ($drop.hasClass("visible")) {
+      setTimeout(function() {
+        $drop.addClass("withBG");
+      }, 400 + options*100);
+    }
+    triggerAnimation();
+    if (val !== "placeholder" || prevActive === "placeholder") {
+      $(".drop .option").removeClass("active");
+      $(this).addClass("active");
+    };
+  });
+
+  function triggerAnimation() {
+    var finalWidth = $(".drop").hasClass("visible") ? 22 : 20;
+    $(".drop").css("width", "24em");
+    setTimeout(function() {
+      $(".drop").css("width", finalWidth + "em");
+    }, 400);
+  }
 });
